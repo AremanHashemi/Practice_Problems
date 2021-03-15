352dbc05dad644d00486e38a5711fd2896ab6a90
Areman <aremanhahsemi@hotmail.com>
2021-03-15 14:48:39 -0700

    Added time measurements

--- Dynamic Programming/Fib.py
+++ Dynamic Programming/Fib.py
@@ -1,10 +1,16 @@
+import time
+
 # Fib without DP
+# O(n^2) time & space
 def fib(n):
   if n <= 1:
     return n
   return fib(n-1) + fib(n-2)
+
 # 0 1 2 3 4 5 6 7
 # 0 1 1 2 3 5 8 13 
+
+start = time.time()
 assert 1 == fib(1)
 assert 1 == fib(2)
 assert 2 == fib(3)
@@ -13,19 +19,31 @@ assert 5 == fib(5)
 assert 8 == fib(6)
 assert 13 == fib(7)
 assert 21 == fib(8)
-print("passed all assertions")
+assert 144 == fib(12)
+assert 2584 == fib(18)
+# assert 125862690254 == fib(50)
+end = time.time()
+print(f"passed all assertions. Time = {end-start}")
 
+# O(n) space & time complexity
 # Fib with DP
 def fib(n, mem=None):
-  if(mem is None):
-    mem = [None] * (n + 1)
+  # Only gets called on first function call
+  if(mem is None): 
+    mem = [None] * (n+1)
+  # Base condition
   if n <= 2:
     return 1
+  # Checking against memory
   if mem[n] is not None:
     return mem[n]
-  return fib(n-1) + fib(n-2)
+  # Otherwise do the calculation and save it
+  mem[n] = fib(n-1, mem) + fib(n-2, mem)
+  return mem[n]
+
 # 0 1 2 3 4 5 6 7
 # 0 1 1 2 3 5 8 13 
+start = time.time()
 assert 1 == fib(1)
 assert 1 == fib(2)
 assert 2 == fib(3)
@@ -34,5 +52,10 @@ assert 5 == fib(5)
 assert 8 == fib(6)
 assert 13 == fib(7)
 assert 21 == fib(8)
-print("passed all assertions")
+assert 144 == fib(12)
+assert 2584 == fib(18)
+assert 12586269025 == fib(50)
+
+end = time.time()
+print(f"passed all assertions. Time = {end-start}")
 
